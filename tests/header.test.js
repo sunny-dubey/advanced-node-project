1731912264b565cfea7546c65e9e1bfc54bfe9fe
Sunny Dubey <sunnydubey2502@gmail.com>
2023-09-25 02:06:12 +0530

    auth test

--- tests/header.test.js
+++ tests/header.test.js
@@ -11,7 +11,7 @@ beforeEach(async () => {
 });
 
 afterEach(async () => {
-  await browser.close();
+  //await browser.close();
 });
 
 test('the header has the correct text', async () => {
@@ -23,6 +23,30 @@ test('clicking login starts oauth flow', async () => {
   await page.click('.right a');
   const url = await page.url();
   expect(url).toMatch(/accounts\.google\.com/);
+});
+
+test.only('When signed in, show logout button', async () => {
+  const id = '650ec847d79a0907f82a655b';
+  const Buffer = require('safe-buffer').Buffer;
+  const sessionObject = {
+    passport: {
+      passport: {
+        user: id,
+      },
+    },
+  };
+  const sessionString = Buffer.from(JSON.stringify(sessionObject)).toString(
+    'base64'
+  );
+  const Keygrip = require('keygrip');
+  const keys = require('../config/keys');
+  const keygrip = new Keygrip([keys.cookieKey]);
+  const sig = keygrip.sign('session=' + sessionString);
+
+  console.log(sessionString, sig);
+  await page.setCookie({ name: 'session', value: sessionString });
+  await page.setCookie({ name: 'session.sig', value: sig });
+  await page.goto('localhost:3000');
 });
 
 jest.setTimeout(30000);
